21654ecf988da7ec3c39657d3cb360deec2bea7d
Sven Hendrikx <sven.hendrikx.20@gmail.com>
2026-02-04 14:38:15 +0100

    Add hostname field

--- modules/networking.nix
+++ modules/networking.nix
@@ -4,6 +4,7 @@
     networking.firewall.trustedInterfaces = [
         "incusbr0"
     ];
+    networking.hostName = "nixos"
     # networking.firewall.allowedUDPPorts = [ 51820 ];
     # networking.firewall.interfaces."wg0".allowedTCPPorts = [ 22 ];
 }
