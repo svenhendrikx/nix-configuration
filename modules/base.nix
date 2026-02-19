1980e9d600a126e59e46b70a9cc6442fb40f5764
Sven Hendrikx <sven.hendrikx.20@gmail.com>
2026-02-19 20:58:29 +0100

    Add experimental features

--- modules/base.nix
+++ modules/base.nix
@@ -3,6 +3,8 @@
     time.timeZone = "Europe/Amsterdam";
     i18n.defaultLocale = "en_US.UTF-8";
 
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
     services.openssh.enable = true;
 
     security.sudo.enable = true;
