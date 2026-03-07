e26865e63e0e30c497c04f0bc9e6e01bed32d153
Sven Hendrikx <sven@kxit.nl>
2026-03-07 15:26:10 +0100

    Open DNS related ports

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
-    networking.firewall.allowedTCPPorts = [ 22 80 443 8000 8069 8443 9000 ];
+    networking.firewall.allowedTCPPorts = [ 22 53 80 443 8000 8053 8069 8080 8443 9000 ];
     networking.firewall.trustedInterfaces = [
         "incusbr0"
     ];
     networking.wireguard.enable = true;
-    networking.firewall.allowedUDPPorts = [ 51820 ];
+    networking.firewall.allowedUDPPorts = [ 53 51820 ];
     # networking.firewall.interfaces."wg0".allowedTCPPorts = [ 22 ];
 }
